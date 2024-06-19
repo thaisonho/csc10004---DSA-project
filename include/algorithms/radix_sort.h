2fc0bcc1330fcc36aa228ac4741f8354e597f551
thaisonho <hothaison2005@gmail.com>
2024-06-19 12:47:22 +0700

    implement radix sort

--- include/algorithms/radix_sort.h
+++ include/algorithms/radix_sort.h
@@ -1,4 +1,12 @@
 #ifndef RADIX_SORT_H
 #define RADIX_SORT_H
 
+#include <cstddef>
+#include <algorithm>
+
+static void __countingSort(int* arr, std::size_t n, int exp);
+static void __countingSort(int* arr, std::size_t n, int exp, unsigned int& cnt);
+void radixSort(int* arr, std::size_t n);
+void radixSort(int* arr, std::size_t n, unsigned int& cnt);
+
 #endif //RADIX_SORT_H
--- src/radix_sort.cpp
+++ src/radix_sort.cpp
@@ -1,2 +1,75 @@
 #include "../include/algorithms/radix_sort.h"
 
+void __countingSort(int* arr, std::size_t n, int exp)
+{
+    int* output = new int[n];
+    int count[10] = {0};
+
+    for(std::size_t i = 0; i < n; ++i) {
+        ++count[(arr[i] / exp) % 10];
+    }
+
+    for(std::size_t i = 1; i < n; ++i) {
+        count[i] += count[i - 1];
+    }
+
+    for(std::size_t i = n - 1; i >= 0; --i) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;   
+    }
+
+    for(std::size_t i = 0; i < n; ++i) {
+        arr[i] = output[i];
+    }
+
+    delete[] output;
+}
+
+void __countingSort(int* arr, std::size_t n, int exp, unsigned int& cnt)
+{
+    cnt = 0;
+    int* output = new int[n];
+    int count[10] = {0};
+
+    for(std::size_t i = 0; ++cnt && i < n; ++i) {
+        int digit = (arr[i] / exp) % 10;
+        ++count[digit];
+    }
+
+    for(std::size_t i = 1; ++cnt && i < n; ++i) {
+        count[i] += count[i - 1];
+        ++cnt;
+    }
+
+    for(std::size_t i = n - 1; ++cnt && i >= 0; --i) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        --count[digit];
+    }
+
+    for(std::size_t i = 0; ++cnt && i < n; ++i) {
+        arr[i] = output[i];
+    }
+
+    delete[] output;
+}
+
+void radixSort(int* arr, std::size_t n)
+{
+    int max = *std::max_element(arr, arr + n);
+    for(int exp = 1; max / exp > 0; exp *= 10) {
+        __countingSort(arr, n, exp);
+    }
+}
+
+void radixSort(int* arr, std::size_t n, unsigned int& cnt)
+{
+    cnt = 0;
+    int max = *std::max_element(arr, arr + n);
+    unsigned int tmpCnt;
+    for(int exp = 1; ++cnt && max / exp > 0; exp *= 10) {
+        __countingSort(arr, n, exp, tmpCnt);
+        cnt += tmpCnt;
+    }
+}
