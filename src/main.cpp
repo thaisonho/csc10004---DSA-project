3e6acb50ef9cb70d5354840b48781b29817d9287
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2024-07-01 22:50:50 +0700

    100%

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    if (argc <= 2)
-        return 0;
+    try
+    {
+        if (argc <= 2) throw -1;
 
-    if (string (argv[1]) == "-a")
-        AlgorithmMode(argc, argv);
-    else if (string (argv[1]) == "-c")
-        ComparisonMode(argc, argv);
-    else
-        cout << "Error!!! Valid Argument!!!\n";
+        if (std::string(argv[1]) == "-a") AlgorithmMode(argc, argv);
+        else if (std::string(argv[1]) == "-c") ComparisonMode(argc, argv);
+        else throw -1;
+    }
+    catch(int x)
+    {
+        if (x == -1)
+        {
+            std::cout << "Error: Invalid arguments!" << std::endl;
+        }
+    }
 
     return 0;
 }
