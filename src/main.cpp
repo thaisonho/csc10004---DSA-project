7152ed42d3c827b9aeac2086b86c2e471d711703
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2024-07-01 22:52:13 +0700

    100.1%

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,14 @@ int main(int argc, char *argv[])
     {
         if (argc <= 2) throw -1;
 
-        if (std::string(argv[1]) == "-a") AlgorithmMode(argc, argv);
-        else if (std::string(argv[1]) == "-c") ComparisonMode(argc, argv);
+        if (string(argv[1]) == "-a") AlgorithmMode(argc, argv);
+        else if (string(argv[1]) == "-c") ComparisonMode(argc, argv);
         else throw -1;
     }
     catch(int x)
     {
         if (x == -1)
-        {
-            std::cout << "Error: Invalid arguments!" << std::endl;
-        }
+           cout << "Error: Invalid arguments!" << '\n';
     }
 
     return 0;
