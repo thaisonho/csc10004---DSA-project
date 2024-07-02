218b00dc36822f70449351bdb682939dedbd8515
NguyenAn0808 <23127148@student.hcmus.edu.vn>
2024-07-02 20:14:44 +0700

    Full code

--- src/ShellSort.cpp
+++ src/ShellSort.cpp
@@ -44,37 +44,3 @@ void ShellSort(int a[], int n, long long& count_compare)
         }
     }
 }
-
-void ShellSortOptimize(int a[], int n)
-{
-    for (int gap = n / 2; gap > 0; gap /= 2)
-    {
-        for (int i = gap; i < n; i++)
-        {
-            int temp = a[i];
-
-            int j = i;
-            for (; j >= gap && a[j - gap] > temp; j -= gap)
-                a[j] = a[j - gap];
-
-            a[j] = temp;
-        }
-    }
-}
-
-void ShellSortOptimize(int a[], int n, long long& count_compare)
-{
-    for (int gap = n / 2; ++count_compare && gap > 0; gap /= 2)
-    {
-        for (int i = gap; ++count_compare && i < n; i++)
-        {
-            int temp = a[i];
-
-            int j = i;
-            for (; ++count_compare && j >= gap && ++count_compare && a[j - gap] > temp; j -= gap)
-                a[j] = a[j - gap];
-
-            a[j] = temp;
-        }
-    }
-}
--- src/ShellSort.h
+++ src/ShellSort.h
@@ -4,9 +4,6 @@
 #include <iostream>
 
 void ShellSort(int a[], int n);
-void ShellSortOptimize(int a[], int n);
-
 void ShellSort(int a[], int n, long long& count_compare);
-void ShellSortOptimize(int a[], int n, long long& count_compare);
 
 #endif 
